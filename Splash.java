c580738998a9de307ecdc4ca71c95200c013bfdf
Danialasyraaf12 <danialasyraaf489@gmail.com>
2026-01-21 21:49:27 +0800

    Refactor Splash screen implementation into multiple classes

--- Splash.java
+++ Splash.java
@@ -1,50 +1,72 @@
 package electricity.billing.system;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class Splash extends JFrame implements Runnable {
-    
-    Thread t;
-    Splash(){
-        
-        ImageIcon i1 = new ImageIcon (ClassLoader.getSystemResource("icon/elect.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(730, 550, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon (i2);
-        JLabel image = new JLabel(i3);
-        add (image);
-        
-        setVisible(true);
-        
+public class Splash extends JFrame {
+
+    public Splash() {
+        // Setup UI
+        UISetup uiSetup = new UISetup();
+        uiSetup.setupUI(this);
+
+        // Run the splash screen animation
+        SplashScreenThread splashThread = new SplashScreenThread(this);
+        splashThread.start();
+
+        // Transition to the Login screen
+        UserAuth userAuth = new UserAuth();
+        userAuth.transitionToLogin();
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
+
+public class SplashScreenThread extends Thread{
+    private JFrame frame;
+
+    public SplashScreenThread(JFrame frame){
+        this.frame = frame;
+    }
+
+    public void run (){
         int x=1;
-        for(int i=2; i<600; i+=4, x+=1){
-            setSize(i+x ,i);
-            setLocation(700 - ((i+x)/2),400 - (i/2));
+        for(int i =2; i<600; i+=4, x+=1){
+            frame.setSize(i + x, i);
+            frame.setLocation(700-((i + x)/2), 400 - 1 (i/2));
             try{
                 Thread.sleep(10);
-            }catch (Exception e){
+            } catch (Exception e){
                 e.printStackTrace();
             }
         }
-        
-        t= new Thread(this);
-        t.start();
-        
-        setVisible(true);
     }
-    
-    public void run(){
-        try{
-            Thread.sleep(5000);
-            setVisible(false);
-            
-            new Login();
-        } catch (Exception e){
+}
+
+public class UserAuth {
+    public void transitionToLogin() {
+        try {
+            Thread.sleep(5000);  // Wait for 5 seconds (simulating splash screen duration)
+            new Login();         // Transition to the Login screen
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public static void main(String[] args){
-        new Splash();
+}
+
+public class UISetup {
+    public void setupUI(JFrame frame) {
+        // Load and scale the image
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(730, 550, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        
+        // Create JLabel and add the image to the frame
+        JLabel image = new JLabel(i3);
+        frame.add(image);
+        
+        // Set frame visibility to true
+        frame.setVisible(true);
     }
 }
